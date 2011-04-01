c4e8d17d759c9de35ddba615e667e91fbd14e72f
Adam Grigolato <balcora1337@gmail.com>
2011-04-02 06:55:45 +1000

    Kernel code with makefile added

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 1 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 PREEMPT Sat Apr 2 05:05:35 EST 2011"
+#define LINUX_COMPILE_TIME "05:05:35"
+#define LINUX_COMPILE_BY "git"
+#define LINUX_COMPILE_HOST "kappa"
+#define LINUX_COMPILE_DOMAIN "gull17.balcora.net"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
